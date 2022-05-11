db444c3099487d5559118d6d88e33ce73758ac52
jailsonrs <jailsonr@alu.ufc.br>
2022-05-11 10:37:01 -0300

    desacoplamento das funcoes

--- Funcoes Auxiliares/R/BuscaAdj/buscaSequencial.R
+++ Funcoes Auxiliares/R/BuscaAdj/buscaSequencial.R
@@ -1,61 +1,80 @@
 library(purrr)
 library(dplyr)
 library(tidyr)
+library(future.apply)
 
-dados10=read.table("/home/jailson/Downloads/dados.txt",
+dados10 = read.table("/home/jailson/Downloads/dados.txt",
   sep=" ", 
   dec=".",
   header = TRUE)
 
-fun <- function(dados){    
-  id_list <- unique(as.character(dados$clientID))
-  output <- data.frame()
-  for (user in id_list){
-    dados %>% filter(clientID == user) -> filtro
-    filtro %>% arrange(hora) -> filtro
-    nrows <- nrow(filtro)
-    if (nrows == 1){output <- rbind(output, filtro); next}
-    output <- rbind(output, buscaSequencial(filtro))
-    print(paste("Linhas no dataframe", nrow(output)))
-  }
-  
-  return(output)
+log = function(d.f, .s){
+    print(nrow(d.f))
+    print("_*__*__*__*__*__*__*__*__*__*_*__*__*__*__*__*__*__*__*__*__")
+    print(d.f)
+    print(.s)
+    print("-*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--")
 }
 
-buscaSequencial <- function(d.f){
-  repetidos <- rep(0, length(d.f$página))
+all_distinct <- function(d.f){
   x = 0
-  if (nrow(d.f)>1){
+  if (nrow(d.f) > 1){
     for (i in 2:nrow(d.f)-1){ 
       x[i] = c(d.f$página[i] != d.f$página[i+1])
     }
   }
-  if (length(unique(d.f$página)) == 1 ){
-    return(d.f[1,])
-  }
-  else if(nrow(d.f) == sum(x)+1) return(d.f)
-  else if (length(unique(d.f$página)) > 1){
-    for (i in (2:nrow(d.f))-1){
+  return(sum(x))
+}
+
+busca_duplicados <- function(d.f){
+  repetidos <- rep(0, length(d.f$página))
+  for (i in (2:nrow(d.f))-1){
     if (d.f$página[i+1] == d.f$página[i]){
       repetidos[c(i, i+1)] <- c(1, -1)     
       print(repetidos)
-      d.f <- d.f[-which(repetidos == -1),]   #trocar por -1?
-      print(nrow(d.f))
-      return (buscaSequencial(d.f))
-    }
-    
-    print("_*__*__*__*__*__*__*__*__*__*_*__*__*__*__*__*__*__*__*__*__")
-    print(repetidos)
-    print(d.f)
-    print(length(repetidos))
-    print("-*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--")
-    }
+      d.f <- d.f[-which(repetidos == -1),]   #trocar por -1? 
+      log(d.f, repetidos)
+      return (remove_duplicados(d.f))
+    } 
   }
-} 
+}
+
+remove_duplicados <- function(d.f){
+  #se todos os elementos sao iguais, basta utilizar o primeiro
+  if (length(unique(d.f$página)) == 1 ) return(d.f[1,])
+
+  #se todos sao distintos dois a dois retorna o df todo
+  else if (nrow(d.f) == all_distinct(d.f)+1 ) return(d.f)
+
+  #se existem pelo menos dois distintos no df chama a func. busca_duplicados
+  else if (length(unique(d.f$página)) > 1 ) busca_duplicados(d.f) 
+}
+
+apply_fun_split <- function(d.f, .f){    
+  all_distinct
+  busca_duplicados
+  remove_duplicados
+  id_list <- unique(as.character(d.f$clientID))
+  output <- data.frame()
+  d.f %>% split(~clientID) %>% 
+          lapply(arrange, hora) -> output
+  output <- do.call(rbind, future_lapply(output, .f))
+  return(output)
+}
 
 
 
-dados10 %>% nest(dados = !data) -> dados10
-lista_semduplicatas  = lapply(dados10$dados, fun)
-df_limpo = do.call(rbind, lista_semduplicatas)
+#separa por data
+df_data = split(dados10, ~data)
+
+#elimina as dulpicatas
+a1 = Sys.time()
+## rodar em paralelo em 5 sessoes do R
+plan(multisession, workers = 5)
+l = future_lapply( df_data,
+                   apply_fun_split, 
+                   remove_duplicados)
+b1 = Sys.time()
+b1-a1
+
 
